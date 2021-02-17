75a39fd2fc6c105e4eca837c9cd0de285af82d73
Little Poses <littleposes@Littles-MacBook-Pro.local>
2021-02-17 00:33:41 -0500

    setup, working index, profile models

---
+++ content/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from content.model_lists import *
+# Create your models here.
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    cell_number = models.CharField(max_length=100,blank=True,null=True)
+    phone_number = models.CharField(max_length=100,blank=True,null=True)
+    email = models.EmailField(max_length=100,blank=True,null=True)
+    street_number = models.CharField(max_length=6, blank=True,null=True)
+    street_name = models.CharField(max_length=100, blank=True,null=True)
+    street_type = models.CharField(verbose_name="Street type",choices=street_choices,max_length=4, blank=True,)
+    location_city = models.CharField(verbose_name="City",max_length=100, blank=True,)
+    location_state = models.CharField(verbose_name="State",choices=state_validation_choices,max_length=2, blank=True,)
+    location_zip = models.CharField(max_length=5, blank=True,null=True)
